0778ec87411eeeac397f8186ce25f2aafaa82bb6
Felipe Cuba <felipeccuba1803@gmail.com>
2023-03-01 00:00:32 -0300

    feat(atividades-aula-02)

--- html/Aula-02/Aula-02-atividade-03/javascript/index.js
+++ html/Aula-02/Aula-02-atividade-03/javascript/index.js
@@ -1,21 +1,43 @@
-const teste = "abc";
 function palindromo(value) {
-  let frase;
-  if (value.includes("-")) {
-    frase = value.split("-").join(" ").toLowerCase().split("");
+  let aux;
+  if (value.includes('-')) {
+    aux = value
+      .split('-')
+      .join(' ')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .split('')
+      .filter((value) => value !== ' ');
   } else {
-    frase = value.toLowerCase().split("");
+    aux = value
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .split('')
+      .filter((value) => value !== ' ');
   }
-
-  frase = frase.filter((item) => item !== " ");
-  const aux = frase;
-  const fraseReversa = frase.reverse();
-
-  console.log(frase);
-  console.log(fraseReversa);
-
-  return JSON.stringify(frase) === JSON.stringify(fraseReversa);
+  const frase = JSON.stringify(aux);
+  const fraseReversa = JSON.stringify(aux.reverse());
+  return frase === fraseReversa;
 }
 
-const result = palindromo(teste);
-console.log(result);
+const frase = prompt('Digite uma frase');
+
+if (frase.length > 0) {
+  const y = +frase;
+
+  if (!isNaN(y)) {
+    alert('Você digitou um número, não uma frase! 😐');
+  } else {
+    if (palindromo(frase)) {
+      alert('Essa frase é um palíndromo! 😃');
+    } else {
+      alert('Essa frase não é uma palíndromo! 🙁');
+    }
+  }
+} else {
+  alert('Digite alguma coisa poxa! 😑');
+}
+
+document.write('Obrigado por visitar a página!');
